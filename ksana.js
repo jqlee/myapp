b27738a48c3654f28f1ebde954b7447d713ca763
jqlee <jiachi.lee@gmail.com>
2015-03-10 16:28:54 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,48 @@
+jsonp_handler({
+ "version": "1",
+ "build": 2,
+ "title": "myapp",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/jqlee/myapp/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  547,
+  64,
+  652507,
+  145170,
+  277980,
+  380,
+  35607,
+  113502,
+  97542
+ ],
+ "filedates": [
+  "2015-03-10T08:25:12.106Z",
+  "2015-03-10T08:25:12.105Z",
+  "2015-03-10T08:25:12.164Z",
+  "2015-03-10T08:26:48.663Z",
+  "2015-03-10T08:25:12.128Z",
+  "2015-03-10T08:25:12.129Z",
+  "2015-03-10T08:25:12.097Z",
+  "2015-03-10T08:25:12.088Z",
+  "2015-03-10T08:26:11.245Z"
+ ],
+ "date": "2015-03-10T08:26:49.632Z"
+})
